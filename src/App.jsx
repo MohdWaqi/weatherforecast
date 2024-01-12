389e8682be0cfb8a0986a34979b3d83941ded5f8
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2024-01-13 00:33:27 +0530

    toggle button designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useRef } from "react"
+import NavBar from "./Components/NavBar"
 
 function App() {
- 
-
+const toggleBtn = useRef(null)
+const handleClick =(e)=>{
+console.log(toggleBtn.current.checked)
+}
   return (
-    <>
+    <div className="app">
+    <div className="home">
+    <NavBar/>
+     
+         <div className="btn-container" >
+   
+      <label className="switch btn-color-mode-switch">
+            <input ref={toggleBtn} type="checkbox" name="color_mode" id="color_mode"onClick={handleClick}/>
+            <label htmlFor="color_mode" data-on="F" data-off="C" className="btn-color-mode-switch-inner"></label>
+        </label>
       
-    </>
+
+  
+      </div>
+    </div>
+
+    </div>
   )
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <h1>Weather Forecast</h1>
+        
+    </nav>
+  )
+}
+
+export default NavBar

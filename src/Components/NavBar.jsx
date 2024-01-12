8667bfb34fc6439e7757b88c1e34ac7c5694566c
Waqi <117337520+MohdWaqi@users.noreply.github.com>
2024-01-13 01:54:10 +0530

    navbar completion

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Navbar.css"
+import search from "../assets/search.svg"
 
 const NavBar = () => {
+    const [isSearch, setSearch] = useState(false)
   return (
     <nav>
         <h1>Weather Forecast</h1>
-        
+        <div onMouseEnter={()=>{setSearch(true)}} onMouseLeave={()=>{setSearch(false)}}>
+            {isSearch&&<input/>}
+            <img src={search} alt='search'/>
+        </div>
     </nav>
   )
 }
